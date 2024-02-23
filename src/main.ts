8691e33b8f333cf362070162fe0f38966d85c4f2
fvankrieken <fvankrieken@planning.nyc.gov>
2024-02-23 15:03:58 -0500

    print statement

--- src/main.ts
+++ src/main.ts
@@ -70,6 +70,8 @@ async function getChangedFiles(token: string, base: string, ref: string, initial
 
   const prEvents = ['pull_request', 'pull_request_review', 'pull_request_review_comment', 'pull_request_target']
   const treatPrAsPush = core.getInput('treat-pr-as-push', {required: false})
+  core.warning(treatPrAsPush)
+  core.warning((!(treatPrAsPush) && prEvents.includes(github.context.eventName)))
   if (!(treatPrAsPush) && prEvents.includes(github.context.eventName)) {
     if (ref) {
       core.warning(`'ref' input parameter is ignored when 'base' is set to HEAD`)
